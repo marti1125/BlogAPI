308e7705659ed7abfd16394e234fe47123f34fc4
Willy Aguirre <marti1125@gmail.com>
2021-03-23 15:46:40 -0300

    added custom permission

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URL conf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
