21b8df553ad13824c5e2f79e7a3a1d55a563117f
Willy Aguirre <marti1125@gmail.com>
2021-03-23 17:12:01 -0300

    added swagger redoc

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,22 @@ Including another URL conf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Blog API',
+        default_version='v1',
+        description='A sample API form learnig DRF',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='will@company.com'),
+        license=openapi.License(name='MIT license'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +38,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name='schema-redoc'),
 ]
